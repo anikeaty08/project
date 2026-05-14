46420d51b2a54a2b160c0416625476c2c4732d09
anikeaty08 <aniketbxr1@gmail.com>
2026-05-15 03:02:42 +0530

    building

--- frontend/src/components/SessionSidebar.jsx
+++ frontend/src/components/SessionSidebar.jsx
@@ -41,7 +41,7 @@ export default function SessionSidebar({
                 onDelete?.(s.id);
               }}
             >
-              ×
+              x
             </button>
           </li>
         ))}
